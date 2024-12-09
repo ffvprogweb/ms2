41e3fe7848bff9a803bc10bb31087cccef4254b7
almeida1 <engsoftware1@gmail.com>
2024-12-09 11:13:42 -0300

    versao inicial

---
+++ sigvsmsemail/src/main/java/com/fatec/sigvsemail/SigvsmsemailApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.sigvsemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigvsmsemailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigvsmsemailApplication.class, args);
+	}
+
+}
---
+++ sigvsmsemail/src/test/java/com/fatec/sigvsemail/SigvsmsemailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.sigvsemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigvsmsemailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
